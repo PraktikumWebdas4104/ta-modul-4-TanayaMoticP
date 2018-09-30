d021b46f92fbb96dbd6c00c6ab65840a14fa06f0
Tanaya Motic Permatasari <43507919+TanayaMoticP@users.noreply.github.com>
2018-09-30 16:03:34 +0700

    Form Awal
    Tanaya Motic Permatasari
    6701174148
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,35 @@
 <?php
+session_start();
+error_reporting(0);
+        $user = array(
+                 "user" => "Tanaya",
+                 "pass"=>"12345"            
+                    );
+if (isset($_POST['submit'])) {
+    if ($_POST['username'] == $user['user'] && $_POST['password'] == $user['pass']){
+       
+        $_SESSION["username"] = $_POST['username']; 
+        echo "Selamat Datang $_POST[username]";
 
-
-
-?>
+    } else {
+        
+        display_login_form();
+        echo '<p>Username Atau Password Tidak Benar</p>';
+    }
+}    
+else { 
+    display_login_form();
+}
+function display_login_form(){ ?>
+    <form action="prosesTA.php" method='POST'>
+    <center> LOGIN HERE FIRST<center><br/>
+    <center><label for="username"> Klik You Username :</label><center>
+    <input type="text" name="username" id="username">
+    <br><br>
+    <center><label for="password">Klik Your Password :</label><center>
+    <input type="password" name="password" id="password">
+    <br><br>
+    <input type="submit" name="submit" value="Login">
+    
+    </form>    
+<?php } ?>
